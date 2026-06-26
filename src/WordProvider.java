d0408e12a2a24b4440ebcf17677f416c7c16e912
ZenTech_DE <vsrillzenkner46@gmail.com>
2026-06-26 15:44:19 +0200

    add more words

--- src/WordProvider.java
+++ src/WordProvider.java
@@ -3,7 +3,7 @@ import java.util.Random;
 public class WordProvider {
 
     private static final String[] WORDS = {
-            "JAVA", "PROGRAMMIEREN", "COMPUTER", "SPIEL", "ENTWICKLUNG"
+            "JAVA", "PROGRAMMIEREN", "COMPUTER", "SPIEL", "ENTWICKLUNG", "SOFTWARE", "HARDWARE", "ALGORITHMUS", "DATENBANK", "NETZWERK","KOMPILIEREN", "DEBUGGING", "FUNKTION", "KLASSE", "OBJEKT", "METHODEN", "VARIABLE", "SCHLEIFE", "BEDINGUNG"
     };
 
     public static String getRandomWord() {
